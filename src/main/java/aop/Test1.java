020513ebe320f969dd672d631eaf538ae387f5c6
AlexanderKodor <informghp3@gmail.com>
2024-06-20 16:36:57 +0300

    Дошёл до 2.6 урока

--- src/main/java/aop/Test1.java
+++ src/main/java/aop/Test1.java
@@ -5,8 +5,14 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class Test1 {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MyConfig.class);
-        Library library = context.getBean("library", Library.class);
-        library.getBook();
+        UniLibrary uniLibrary = context.getBean("uniLibrary", UniLibrary.class);
+//        Book book = context.getBean("book", Book.class);
+        uniLibrary.getBook();
+        uniLibrary.returnMagazine();
+        uniLibrary.addBook();
+//        uniLibrary.returnBook();
+//        SchoolLibrary schoolLibrary = context.getBean("schoolLibrary", SchoolLibrary.class);
+//        schoolLibrary.getBook();
 
         context.close();
     }
---
+++ src/main/java/aop/UniLibrary.java
@@ -0,0 +1,28 @@
+package aop;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class UniLibrary extends AbstractLibrary {
+
+    public void getBook(){
+        System.out.println("Мы берем книгу из UniLibrary: ");
+    }
+
+    public void returnBook(){
+        System.out.println("Мы возвращаем книгу из UniLibrary.");
+    }
+    public void addBook(){
+        System.out.println("Мы добавляем книгу из UniLibrary: ");
+    }
+
+    public void getMagazine(){
+        System.out.println("Мы берем журнал из UniLibrary.");
+    }
+    public void returnMagazine(){
+        System.out.println("Мы возвращаем журнал из UniLibrary.");
+    }
+    public void addMagazine(){
+        System.out.println("Мы добавляем журнал из UniLibrary: ");
+    }
+}
